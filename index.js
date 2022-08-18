2c2a7a783060d091871e5c7d22198d1eb2d2708e
altamash-sayyed <altamashsayyed786313@gmail.com>
2022-08-18 20:13:16 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors =require('cors')
 const cookiparser = require("cookie-parser");
-
+require('dotenv').config()
 const app = express()
 const db = require ('./db.js')
 
@@ -14,8 +14,10 @@ app.use(cookiparser());
 app.use(cors())
 
 
-   
+   app.get('/',(req,res)=>{
+res.send("success")
+   })
 
-
+const port = process.env.PORT || 5000
 app.use('/api/loan', require('./routes/routes.js'))
-  app.listen(5000, () => console.log('app is listen on port 5000'))
+  app.listen(port, () => console.log('app is listen on port 5000'))
